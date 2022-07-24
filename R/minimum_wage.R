96f46206189a61068bd6d6d940bbe3ab6e23af45
italian_goat <81044007+niyangbai@users.noreply.github.com>
2022-07-24 22:19:50 +0200

    update

--- R/minimum_wage.R
+++ R/minimum_wage.R
@@ -6,3 +6,25 @@
 #'
 #' @source <https://www.dol.gov/agencies/whd/state/minimum-wage/history>
 "minimum_wage"
+
+#' Find difference of minimum wage
+#'
+#' @description Find difference of minimum wage.
+#'
+#' @param start_year The start year
+#' @param end_year The end year
+#' @param data Minimum wage
+#'
+#' @return The difference of minimum wage
+#' @export
+#'
+#' @examples
+#' find_difs(2010, 2015)
+find_difs <- function(start_year, end_year, data = minwage_state_year) {
+  starts <- data[which(data[["year"]] == start_year),]
+  ends <- data[which(data[["year"]] == end_year),]
+  data <- merge(starts, ends, by = "state")
+  df <- data["state"]
+  df$dif <- data$State.Minimum.Wage.x - data$State.Minimum.Wage.y
+  return(df)
+}
